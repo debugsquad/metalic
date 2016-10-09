71d06c721730b99330e1407f25b74780b50c3a56
vauxhall <zero.griffin@gmail.com>
2016-10-09 17:40:37 -0500

    model main

--- metalic/Model/Main/MMain.swift
+++ metalic/Model/Main/MMain.swift
@@ -1,9 +1,44 @@
-//
-//  MMain.swift
-//  metalic
-//
-//  Created by zero on 10/9/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MMain
+{
+    let items:[MMainItem]
+    var state:MMenuState
+    weak var current:MMenuItem!
+    weak var itemHome:MMenuItemHome!
+    
+    init()
+    {
+        state = MMenuStateOptions()
+        
+        var items:[MMenuItem] = []
+        
+        let itemSettings:MMenuItemSettings = MMenuItemSettings(index:items.count)
+        items.append(itemSettings)
+        
+        let itemHistory:MMenuItemHistory = MMenuItemHistory(index:items.count)
+        items.append(itemHistory)
+        
+        let itemHome:MMenuItemHome = MMenuItemHome(index:items.count)
+        current = itemHome
+        self.itemHome = itemHome
+        items.append(itemHome)
+        
+        let itemRooms:MMenuItemRooms = MMenuItemRooms(index:items.count)
+        items.append(itemRooms)
+        
+        self.items = items
+    }
+    
+    //MARK: public
+    
+    func pushed()
+    {
+        state = MMenuStatePushed()
+    }
+    
+    func poped()
+    {
+        state = MMenuStateOptions()
+    }
+}
