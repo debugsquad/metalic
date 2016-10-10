d03fae95f78c42ebe6a7d8794d58753a4e9fa856
vauxhall <zero.griffin@gmail.com>
2016-10-10 18:09:54 -0500

    imageView in image home

--- metalic/View/Home/VHomePicture.swift
+++ metalic/View/Home/VHomePicture.swift
@@ -3,6 +3,7 @@ import UIKit
 class VHomePicture:UIView
 {
     weak var controller:CHome!
+    weak var imageView:UIImageView!
     
     convenience init(controller:CHome)
     {
@@ -11,5 +12,30 @@ class VHomePicture:UIView
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "imageView":imageView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
