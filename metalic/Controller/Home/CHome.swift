b071123ce8f1de5b6bd329ca4c6f843d26c4f727
vauxhall <zero.griffin@gmail.com>
2016-10-10 18:22:03 -0500

    mtl device created

--- metalic/Controller/Home/CHome.swift
+++ metalic/Controller/Home/CHome.swift
@@ -4,12 +4,20 @@ import Metal
 class CHome:CController
 {
     weak var viewHome:VHome!
+    private var mtlDevice:MTLDevice?
     
     override func loadView()
     {
         let viewHome:VHome = VHome(controller:self)
         self.viewHome = viewHome
         view = viewHome
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        mtlDevice = MTLCreateSystemDefaultDevice()
     }
     
     //MARK: public
