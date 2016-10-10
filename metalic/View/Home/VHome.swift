cc741c4ee9d96ee5823cd134b5d1ea846f631986
vauxhall <zero.griffin@gmail.com>
2016-10-10 16:50:39 -0500

    view menu and picture in view home

--- metalic/View/Home/VHome.swift
+++ metalic/View/Home/VHome.swift
@@ -3,6 +3,8 @@ import UIKit
 class VHome:UIView
 {
     weak var controller:CHome!
+    weak var viewPicture:VHomePicture!
+    weak var viewMenu:VHomeMenu!
     private let kMenuHeight:CGFloat = 80
     
     convenience init(controller:CHome)
@@ -12,5 +14,37 @@ class VHome:UIView
         clipsToBounds = true
         backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = false
+        
+        let viewPicture:VHomePicture = VHomePicture(controller:controller)
+        self.viewPicture = viewPicture
+        
+        let viewMenu:VHomeMenu = VHomeMenu(controller:controller)
+        self.viewMenu = viewMenu
+        
+        addSubview(viewPicture)
+        addSubview(viewMenu)
+        
+        let views:[String:UIView] = [
+            "viewPicture":viewPicture,
+            "viewMenu":viewMenu]
+        
+        let metrics:[String:CGFloat] = [
+            "menuHeight":kMenuHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[viewPicture]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[viewMenu]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[viewPicture]-0-[viewMenu(menuHeight)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
