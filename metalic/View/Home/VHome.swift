0d75c63017e38e2ec225e4a5ad6565e6d7287791
vauxhall <zero.griffin@gmail.com>
2016-10-10 16:54:46 -0500

    selector constraints

--- metalic/View/Home/VHome.swift
+++ metalic/View/Home/VHome.swift
@@ -4,6 +4,7 @@ class VHome:UIView
 {
     weak var controller:CHome!
     weak var viewPicture:VHomePicture!
+    weak var viewSelector:VHomeSelector!
     weak var viewMenu:VHomeMenu!
     private let kSelectorHeight:CGFloat = 60
     private let kMenuHeight:CGFloat = 100
@@ -19,21 +20,32 @@ class VHome:UIView
         let viewPicture:VHomePicture = VHomePicture(controller:controller)
         self.viewPicture = viewPicture
         
+        let viewSelector:VHomeSelector = VHomeSelector(controller:controller)
+        self.viewSelector = viewSelector
+        
         let viewMenu:VHomeMenu = VHomeMenu(controller:controller)
         self.viewMenu = viewMenu
         
         addSubview(viewPicture)
+        addSubview(viewSelector)
         addSubview(viewMenu)
         
         let views:[String:UIView] = [
             "viewPicture":viewPicture,
+            "viewSelector":viewSelector,
             "viewMenu":viewMenu]
         
         let metrics:[String:CGFloat] = [
+            "selectorHeight":kSelectorHeight,
             "menuHeight":kMenuHeight]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[viewPicture]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[viewSelector]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -43,7 +55,7 @@ class VHome:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[viewPicture]-0-[viewMenu(menuHeight)]-0-|",
+            withVisualFormat:"V:|-0-[viewPicture]-0-[viewSelector(selectorHeight)]-0-[viewMenu(menuHeight)]-0-|",
             options:[],
             metrics:metrics,
             views:views))
