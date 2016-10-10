46a796b425f316bdff71b365eb36163144aa5efa
vauxhall <zero.griffin@gmail.com>
2016-10-10 17:00:36 -0500

    buttons visual constraints

--- metalic/Extensions/ExtensionUiColor.swift
+++ metalic/Extensions/ExtensionUiColor.swift
@@ -17,4 +17,12 @@ extension UIColor
             return UIColor(red:0.7, green:0.8, blue:0.1, alpha:1)
         }
     }
+    
+    open class var background:UIColor
+    {
+        get
+        {
+            return UIColor(white:0.97, alpha:1)
+        }
+    }
 }
--- metalic/View/Home/VHomeSelector.swift
+++ metalic/View/Home/VHomeSelector.swift
@@ -3,13 +3,55 @@ import UIKit
 class VHomeSelector:UIView
 {
     weak var controller:CHome!
+    weak var layoutButtonsLeft:NSLayoutConstraint!
+    weak var layoutButtonCameraTop:NSLayoutConstraint!
+    weak var layoutButtonLibraryTop:NSLayoutConstraint!
+    private let kButtonWidth:CGFloat = 80
+    private let kButtonHeight:CGFloat = 40
     
     convenience init(controller:CHome)
     {
         self.init()
         self.controller = controller
         clipsToBounds = true
-        backgroundColor = UIColor.clear
+        backgroundColor = UIColor.background
         translatesAutoresizingMaskIntoConstraints = false
+        
+        let buttonCamera:UIButton = UIButton()
+        buttonCamera.translatesAutoresizingMaskIntoConstraints = false
+        buttonCamera.clipsToBounds = true
+        buttonCamera.setTitle("Camera", for:UIControlState.normal)
+        
+        let buttonLibrary:UIButton = UIButton()
+        buttonLibrary.translatesAutoresizingMaskIntoConstraints = false
+        buttonLibrary.clipsToBounds = true
+        buttonLibrary.setTitle("Library", for:UIControlState.normal)
+        
+        addSubview(buttonCamera)
+        addSubview(buttonLibrary)
+        
+        let views:[String:UIView] = [
+            "buttonCamera":buttonCamera,
+            "buttonLibrary":buttonLibrary]
+        
+        let metrics:[String:CGFloat] = [
+            "buttonWidth":kButtonWidth,
+            "buttonHeight":kButtonHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[buttonCamera(buttonWidth)]-0-[buttonLibrary(buttonWidth)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[buttonCamera(buttonHeight)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[buttonLibrary(buttonHeight)]",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
