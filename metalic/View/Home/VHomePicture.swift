8d38e50770587db4f354e1093b379b46dcf7b445
vauxhall <zero.griffin@gmail.com>
2016-10-10 16:47:50 -0500

    view home picture

--- metalic/View/Home/VHomePicture.swift
+++ metalic/View/Home/VHomePicture.swift
@@ -1,16 +1,15 @@
 import UIKit
 
-class VHome:UIView
+class VHomePicture:UIView
 {
     weak var controller:CHome!
-    private let kMenuHeight:CGFloat = 80
     
     convenience init(controller:CHome)
     {
         self.init()
         self.controller = controller
         clipsToBounds = true
-        backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
     }
 }
