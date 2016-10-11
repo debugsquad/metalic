999f16f2fa2079176e023d839d1e4233d79901a9
vauxhall <zero.griffin@gmail.com>
2016-10-11 10:48:25 -0500

    command buffer

--- metalic/Controller/Home/CHome.swift
+++ metalic/Controller/Home/CHome.swift
@@ -5,6 +5,9 @@ class CHome:CController
 {
     weak var viewHome:VHome!
     var mtlDevice:MTLDevice!
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    var timer: CADisplayLink! = nil
     
     override func loadView()
     {
@@ -18,6 +21,60 @@ class CHome:CController
         super.viewDidLoad()
         
         mtlDevice = MTLCreateSystemDefaultDevice()
+        
+        
+        // 1
+        let defaultLibrary = mtlDevice.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.makeFunction(name:"basic_fragment")
+        let vertexProgram = defaultLibrary!.makeFunction(name:"basic_vertex")
+        
+        // 2
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormat.bgra8Unorm
+        
+        // 3
+        do
+        {
+            try pipelineState = mtlDevice.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        }
+        catch
+        {
+        }
+        
+        commandQueue = mtlDevice.makeCommandQueue()
+        
+        
+        timer = CADisplayLink(target:self, selector:#selector(gameloop(sender:)))
+        timer.add(to:RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+    }
+    
+    func gameloop(sender timer:CADisplayLink)
+    {
+        autoreleasepool {
+            self.render()
+        }
+    }
+    
+    func render() {
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        let drawable = viewHome.viewPicture.metalLayer.nextDrawable()!
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor:renderPassDescriptor)
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(viewHome.viewPicture.vertexBuffer, offset:0, at:0)
+        renderEncoder.drawPrimitives(type: MTLPrimitiveType.triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoder.endEncoding()
+        
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
     }
     
     //MARK: public
