79d948c5c494221a1fec49bcdef0ee2bb98080e8
vauxhall <zero.griffin@gmail.com>
2016-10-12 15:35:07 -0500

    normalized image

--- metalic/Controller/Home/CHome.swift
+++ metalic/Controller/Home/CHome.swift
@@ -4,7 +4,7 @@ import MetalKit
 class CHome:CController
 {
     weak var viewHome:VHome!
-    
+    var normalizedImage:UIImage?
     let device = MTLCreateSystemDefaultDevice()!
     var commandQueue: MTLCommandQueue!
     var sourceTexture: MTLTexture?
@@ -30,6 +30,15 @@ class CHome:CController
     
     //MARK: private
     
+    private func normalize(image:UIImage)
+    {
+        UIGraphicsBeginImageContextWithOptions(image.size, true, image.scale)
+        let rect = CGRect(origin:CGPoint.zero, size:image.size)
+        image.draw(in:rect)
+        normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
     private func setupMetal() {
         commandQueue = device.makeCommandQueue()
         
@@ -44,7 +53,6 @@ class CHome:CController
          For the video image, the content only needs to be filtered whenever the camera provides a new video frame.
          */
         viewHome.viewPicture.isPaused = true
-        viewHome.viewPicture.delegate = self
         viewHome.viewPicture.device = device
         viewHome.viewPicture.colorPixelFormat = .bgra8Unorm
     }
@@ -53,30 +61,56 @@ class CHome:CController
     
     func imageSelected(image:UIImage?)
     {
-        sourceTexture = nil
-        
-        let loader = MTKTextureLoader(device: device)
-        let cgImage = image?.cgImage
-        // The still image is loaded directly into GPU-accessible memory that is only ever read from.
-        
-        var options = [
-            
-            MTKTextureLoaderOptionTextureUsage:         MTLTextureUsage.shaderRead.rawValue,
-            MTKTextureLoaderOptionSRGB:                 0
-        ]
-        
-        if #available(iOS 10.0, *)
+        if image != nil
         {
-            options[MTKTextureLoaderOptionTextureStorageMode] = MTLStorageMode.private.rawValue
+            
+            
+            
+            
+            let cgImage = retVal.cgImage!
+            
+            let scale = UIScreen.main.scale
+            let width = viewHome.viewPicture.bounds.width * scale
+            let height = viewHome.viewPicture.bounds.height * scale
+            let bitsPerComponent = cgImage.bitsPerComponent
+            let bytesPerRow = cgImage.bytesPerRow
+            let colorSpace = cgImage.colorSpace
+            let bitmapInfo = cgImage.bitmapInfo
+            let context = CGContext.init(data:nil, width:Int(width), height:Int(height), bitsPerComponent:bitsPerComponent, bytesPerRow:bytesPerRow, space:colorSpace!, bitmapInfo:bitmapInfo.rawValue)
+            
+            
+            
+            
+            context?.interpolationQuality = CGInterpolationQuality.high
+            context?.draw(cgImage, in:CGRect(origin:CGPoint.zero, size:CGSize(width:width, height:height)))
+            
+            let scaledImage:CGImage? = context?.makeImage()
+            
+            sourceTexture = nil
+            
+            let loader = MTKTextureLoader(device: device)
+            // The still image is loaded directly into GPU-accessible memory that is only ever read from.
+            
+            
+            var options = [
+                
+                MTKTextureLoaderOptionTextureUsage:         MTLTextureUsage.shaderRead.rawValue,
+                MTKTextureLoaderOptionSRGB:                 0
+            ]
+            
+            if #available(iOS 10.0, *)
+            {
+                options[MTKTextureLoaderOptionTextureStorageMode] = MTLStorageMode.private.rawValue
+            }
+            
+            do {
+                let fileTexture = try loader.newTexture(with: scaledImage!, options: options as [String : NSObject]?)
+                sourceTexture = fileTexture
+            } catch let error as NSError {
+                print("Error loading still image texture: \(error)")
+            }
+            
+            viewHome.viewPicture.draw()
         }
-        
-        do {
-            let fileTexture = try loader.newTexture(with: cgImage!, options: options as [String : NSObject]?)
-            sourceTexture = fileTexture
-        } catch let error as NSError {
-            print("Error loading still image texture: \(error)")
-        }
-        
-        viewHome.viewPicture.draw()
     }
 }
