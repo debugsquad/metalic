746f76589790cee0bc03fc24dc1c58815d760734
vauxhall <zero.griffin@gmail.com>
2016-10-14 13:21:53 -0500

    filter none

---
+++ metalic/Model/Filters/Basic/MFiltersItemBasicNone.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class MFiltersItemBasicNone:MFiltersItem
+{
+    private let kImageName:String = ""
+    
+    init()
+    {
+        let name:String = NSLocalizedString("MFiltersItemBasicNone_name", comment:"")
+        let filter:MetalFilter.Type = MetalFilterBasicInk.self
+        
+        super.init(name:name, asset:kImageName, filter:filter)
+    }
+}
