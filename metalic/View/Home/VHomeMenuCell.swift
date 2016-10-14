ecbe08786cc7e1187db673ecb29ed6fa8a723f2f
vauxhall <zero.griffin@gmail.com>
2016-10-14 13:08:55 -0500

    hover cell

--- metalic/View/Home/VHomeMenuCell.swift
+++ metalic/View/Home/VHomeMenuCell.swift
@@ -2,11 +2,38 @@ import UIKit
 
 class VHomeMenuCell:UICollectionViewCell
 {
+    weak var label:UILabel!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.clear
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.textAlignment = NSTextAlignment.center
+        label.font = UIFont.bold(size:14)
+        self.label = label
+        
+        addSubview(label)
+        
+        let views:[String:UIView] = [
+            "label":label]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
@@ -14,9 +41,43 @@ class VHomeMenuCell:UICollectionViewCell
         fatalError()
     }
     
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            backgroundColor = UIColor.main
+            label.textColor = UIColor.white
+        }
+        else
+        {
+            backgroundColor = UIColor.clear
+            label.textColor = UIColor(white:0.8, alpha:1)
+        }
+    }
+    
     //MARK: public
     
     func config(model:MFiltersItem)
     {
+        label.text = model.name
+        hover()
     }
 }
