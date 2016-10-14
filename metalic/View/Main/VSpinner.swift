b859e27a04205b3198ea135c58845a712aa595cc
vauxhall <zero.griffin@gmail.com>
2016-10-14 14:24:56 -0500

    view spinner

--- metalic/View/Main/VSpinner.swift
+++ metalic/View/Main/VSpinner.swift
@@ -1,6 +1,28 @@
 import UIKit
 
-class VSpinner:UIView
+class VSpinner:UIImageView
 {
+    private let kAnimationDuration:TimeInterval = 1
     
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        
+        let images:[UIImage] = [
+        ]
+        
+        isUserInteractionEnabled = false
+        translatesAutoresizingMaskIntoConstraints = false
+        clipsToBounds = true
+        animationDuration = kAnimationDuration
+        animationImages = images
+        contentMode = UIViewContentMode.center
+        
+        startAnimating()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
