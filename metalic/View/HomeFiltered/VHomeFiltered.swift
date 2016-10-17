322fce51a88cb944ffd472d908523d82beac7160
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:07:06 -0500

    view home filtered init

--- metalic/View/HomeFiltered/VHomeFiltered.swift
+++ metalic/View/HomeFiltered/VHomeFiltered.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VHomeFiltered:UIView
 {
+    weak var controller:CHomeFiltered!
     
+    convenience init(controller:CHomeFiltered)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.white
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }
