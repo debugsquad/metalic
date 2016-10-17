1ba5d52e1020a21b8fe725ae45d580fc0948e47e
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:48:42 -0500

    view picture constraints

--- metalic/View/HomeFiltered/VHomeFiltered.swift
+++ metalic/View/HomeFiltered/VHomeFiltered.swift
@@ -12,5 +12,26 @@ class VHomeFiltered:UIView
         backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let viewPicture:VHomeFilteredPicture = VHomeFilteredPicture(controller:controller)
+        self.viewPicture = viewPicture
+        
+        addSubview(viewPicture)
+        
+        let views:[String:UIView] = [
+            "viewPicture":viewPicture]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[viewPicture]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[viewPicture]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
