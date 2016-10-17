1a13a462f822ede7605f1bed2a7ca6b82ba5fc20
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:58:01 -0500

    changed view picture for image view

--- metalic/View/HomeFiltered/VHomeFiltered.swift
+++ metalic/View/HomeFiltered/VHomeFiltered.swift
@@ -3,7 +3,6 @@ import UIKit
 class VHomeFiltered:UIView
 {
     weak var controller:CHomeFiltered!
-    weak var viewPicture:VHomeFilteredPicture!
     
     convenience init(controller:CHomeFiltered)
     {
@@ -13,23 +12,26 @@ class VHomeFiltered:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
-        let viewPicture:VHomeFilteredPicture = VHomeFilteredPicture(controller:controller)
-        self.viewPicture = viewPicture
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
         
-        addSubview(viewPicture)
+        addSubview(imageView)
         
         let views:[String:UIView] = [
-            "viewPicture":viewPicture]
+            "imageView":imageView]
         
         let metrics:[String:CGFloat] = [:]
         
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-0-[viewPicture]-0-|",
+            withVisualFormat:"H:|-0-[imageView]-0-|",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[viewPicture]-0-|",
+            withVisualFormat:"V:|-0-[imageView]-0-|",
             options:[],
             metrics:metrics,
             views:views))
