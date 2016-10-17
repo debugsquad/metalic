febc0f14d35f4ed397e875e6866798344a939377
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:13:23 -0500

    controller filtered init

--- metalic/Controller/Home/CHomeFiltered.swift
+++ metalic/Controller/Home/CHomeFiltered.swift
@@ -1,8 +1,26 @@
 import UIKit
+import MetalKit
 
 class CHomeFiltered:CController
 {
     weak var viewFiltered:VHomeFiltered!
+    weak var home:CHome!
+    let texture:MTLTexture
+    let filter:MetalFilter
+    
+    init(home:CHome, texture:MTLTexture, filter:MetalFilter)
+    {
+        self.texture = texture
+        self.filter = filter
+        
+        super.init(nibName:nil, bundle:nil)
+        self.home = home
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
     
     override func loadView()
     {
