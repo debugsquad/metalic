a90ad5563cdb22d112b59db05e9c783431c72af3
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:08:22 -0500

    loading view in controller

--- metalic/Controller/Home/CHomeFiltered.swift
+++ metalic/Controller/Home/CHomeFiltered.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CHomeFiltered:CController
 {
+    weak var viewFiltered:VHomeFiltered!
     
+    override func loadView()
+    {
+        let viewFiltered:VHomeFiltered = VHomeFiltered(controller:self)
+        self.viewFiltered = viewFiltered
+        view = viewFiltered
+    }
 }
