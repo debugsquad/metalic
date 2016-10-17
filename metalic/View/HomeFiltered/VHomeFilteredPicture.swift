36650425b03d4429eefcd74a10a2e2cd762ed20c
vauxhall <zero.griffin@gmail.com>
2016-10-17 13:18:10 -0500

    view home filtered picture

--- metalic/View/HomeFiltered/VHomeFilteredPicture.swift
+++ metalic/View/HomeFiltered/VHomeFilteredPicture.swift
@@ -1,7 +1,44 @@
 import UIKit
 import MetalKit
 
-class VHomeFilteredPicture
+class VHomeFilteredPicture:MTKView
 {
+    weak var controller:CHomeFiltered!
     
+    convenience init(controller:CHomeFiltered)
+    {
+        self.init()
+        self.controller = controller
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        contentMode = UIViewContentMode.scaleAspectFill
+        autoResizeDrawable = false
+        framebufferOnly = false
+        isPaused = true
+        colorPixelFormat = MTLPixelFormat.bgra8Unorm
+    }
+    
+    override func draw()
+    {
+        guard
+            
+            let drawable:CAMetalDrawable = currentDrawable
+            
+            else
+        {
+            return
+        }
+        
+        let destinationTexture:MTLTexture = drawable.texture
+        let commandBuffer:MTLCommandBuffer = controller.home.commandQueue.makeCommandBuffer()
+        
+        controller.filter.encode(
+            commandBuffer:commandBuffer,
+            sourceTexture:controller.texture,
+            destinationTexture:destinationTexture)
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+        
+        super.draw()
+    }
 }
