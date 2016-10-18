a980bf4f96f1af15fc50224807dc3b2ade22a537
vauxhall <zero.griffin@gmail.com>
2016-10-18 10:40:49 -0500

    encoding chalk filter

--- metalic/Metal/Basic/MetalFilterBasicChalk.swift
+++ metalic/Metal/Basic/MetalFilterBasicChalk.swift
@@ -2,10 +2,27 @@ import MetalPerformanceShaders
 
 class MetalFilterBasicChalk:MetalFilter
 {
+    private let sobelFilter:MPSImageSobel
     private let kFunctionName:String = "filter_basicChalk"
     
     required init(device:MTLDevice)
     {
+        sobelFilter = MPSImageSobel(device:device)
+        sobelFilter.edgeMode = MPSImageEdgeMode.zero
+        
         super.init(device:device, functionName:kFunctionName)
+    }
+    
+    override func encode(commandBuffer:MTLCommandBuffer, sourceTexture:MTLTexture, destinationTexture:MTLTexture)
+    {
+        sobelFilter.encode(
+            commandBuffer:commandBuffer,
+            sourceTexture:sourceTexture,
+            destinationTexture:destinationTexture)
+        
+        super.encode(
+            commandBuffer:commandBuffer,
+            sourceTexture:sourceTexture,
+            destinationTexture:destinationTexture)
     }
 }
