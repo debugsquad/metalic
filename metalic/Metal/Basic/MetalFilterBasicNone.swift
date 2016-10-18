149933a065eab2a3fc1c839d049a16ce10e6b043
vauxhall <zero.griffin@gmail.com>
2016-10-18 09:50:34 -0500

    none filter uses blit encoder

--- metalic/Metal/Basic/MetalFilterBasicNone.swift
+++ metalic/Metal/Basic/MetalFilterBasicNone.swift
@@ -2,8 +2,31 @@ import MetalPerformanceShaders
 
 class MetalFilterBasicNone:MetalFilter
 {
+    private let kDepth:Int = 1
+    
     required init(device:MTLDevice)
     {
         super.init(device:device, functionName:nil)
+    }
+    
+    override func encode(commandBuffer:MTLCommandBuffer, sourceTexture:MTLTexture, destinationTexture:MTLTexture)
+    {
+        let width:Int = sourceTexture.width
+        let height:Int = sourceTexture.height
+        let origin:MTLOrigin = MTLOriginMake(0, 0, 0)
+        let size:MTLSize = MTLSizeMake(width, height, kDepth)
+        let blitEncoder:MTLBlitCommandEncoder = commandBuffer.makeBlitCommandEncoder()
+        
+        blitEncoder.copy(
+            from:sourceTexture,
+            sourceSlice:0,
+            sourceLevel:0,
+            sourceOrigin:origin,
+            sourceSize:size,
+            to:destinationTexture,
+            destinationSlice:0,
+            destinationLevel:0,
+            destinationOrigin:origin)
+        blitEncoder.endEncoding()
     }
 }
