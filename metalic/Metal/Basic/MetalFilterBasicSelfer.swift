4a25555b6fc2414544b6cd8c3e195f85d72831ea
vauxhall <zero.griffin@gmail.com>
2016-10-19 12:17:30 -0500

    faces texture

--- metalic/Metal/Basic/MetalFilterBasicSelfer.swift
+++ metalic/Metal/Basic/MetalFilterBasicSelfer.swift
@@ -10,6 +10,7 @@ class MetalFilterBasicSelfer:MetalFilter
     private let kFacesTextureIndex:Int = 2
     private let kMinImageSize:Int = 640
     private let kMinBokehSize:Int = 15
+    private let kRepeatingElement:Float = 1
     
     required init(device:MTLDevice)
     {
@@ -22,7 +23,7 @@ class MetalFilterBasicSelfer:MetalFilter
     {
         generateDilate(sourceTexture:sourceTexture)
         
-        dilate.encode(commandBuffer:commandBuffer,
+        dilate?.encode(commandBuffer:commandBuffer,
                       sourceTexture:sourceTexture,
                       destinationTexture:destinationTexture)
         
@@ -55,16 +56,29 @@ class MetalFilterBasicSelfer:MetalFilter
             return
         }
         
+        let sourceWidth:Int = sourceTexture.width
+        let sourceHeight:Int = sourceTexture.height
+        let sourceSize:Int = sourceWidth * sourceHeight
+        let sizeOfFloat:Int = MemoryLayout.size(ofValue:kRepeatingElement)
+        var textureArray:[Float] = Array(
+            repeating:kRepeatingElement,
+            count:sourceSize)
+        let bytesPerRow:Int = sizeOfFloat * sourceWidth
+        let region:MTLRegion = MTLRegionMake2D(
+            0,
+            0,
+            sourceWidth,
+            sourceHeight)
+        
         let textureDescriptor:MTLTextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(
             pixelFormat:MTLPixelFormat.r32Float,
-            width:sourceTexture.width,
-            height:sourceTexture.height,
+            width:sourceWidth,
+            height:sourceHeight,
             mipmapped:false)
         
         let facesTexture:MTLTexture = device.makeTexture(descriptor:textureDescriptor)
         let features:[CIFeature] = detector.features(in:image)
-        let sizeOfFloat:Int = MemoryLayout.size(ofValue:kRepeatingValue)
-        let sourceHeight:Int = sourceTexture.height
+        let bokehSizeFloat:Float = Float(bokehSize)
         
         for feature:CIFeature in features
         {
@@ -74,25 +88,81 @@ class MetalFilterBasicSelfer:MetalFilter
                 let faceFeatureW:Int = Int(faceFeature.bounds.size.width)
                 let faceFeatureH:Int = Int(faceFeature.bounds.size.height)
                 let faceFeatureY:Int = sourceHeight - (Int(faceFeature.bounds.origin.y) + faceFeatureH)
-                let regionSize:Int = faceFeatureW * faceFeatureH
-                let bytesPerRow:Int = sizeOfFloat * faceFeatureW
-                let region:MTLRegion = MTLRegionMake2D(
-                    faceFeatureX,
-                    faceFeatureY,
-                    faceFeatureW,
-                    faceFeatureH)
-                let floatArray:[Float] = Array(
-                    repeating:kRepeatingValue,
-                    count:regionSize)
                 
-                let bytes:UnsafeRawPointer = UnsafeRawPointer(floatArray)
-                facesTexture.replace(
-                    region:region,
-                    mipmapLevel:0,
-                    withBytes:bytes,
-                    bytesPerRow:bytesPerRow)
+                for radius:Int in 1 ..< bokehSize
+                {
+                    let radiusFloat:Float = Float(radius)
+                    let thisPixel:Float = radiusFloat / bokehSizeFloat
+                    var minX:Int = faceFeatureX - radius
+                    var maxX:Int = faceFeatureX + faceFeatureW + radius
+                    var minY:Int = faceFeatureY - radius
+                    var maxY:Int = faceFeatureY + faceFeatureH + radius
+                    
+                    if minX < 0
+                    {
+                        minX = 0
+                    }
+                    
+                    if maxX > sourceWidth
+                    {
+                        maxX = sourceWidth
+                    }
+                    
+                    if minY < 0
+                    {
+                        minY = 0
+                    }
+                    
+                    if maxY > sourceHeight
+                    {
+                        maxY = sourceHeight
+                    }
+                    
+                    for indexHr:Int in minX ..< maxX
+                    {
+                        let topIndex:Int = (sourceWidth * minY) + indexHr
+                        let bottomIndex:Int = (sourceWidth * maxY) + indexHr
+                        let currentTop:Float = textureArray[topIndex]
+                        let currentBottom:Float = textureArray[bottomIndex]
+                        
+                        if currentTop > thisPixel
+                        {
+                            textureArray[topIndex] = thisPixel
+                        }
+                        
+                        if currentBottom > thisPixel
+                        {
+                            textureArray[bottomIndex] = thisPixel
+                        }
+                    }
+                    
+                    for indexVr:Int in minY ..< maxY
+                    {
+                        let leftIndex:Int = (sourceWidth * indexVr) + minX
+                        let rightIndex:Int = (sourceWidth * indexVr) + maxX
+                        let currentLeft:Float = textureArray[leftIndex]
+                        let currentRight:Float = textureArray[rightIndex]
+                        
+                        if currentLeft > thisPixel
+                        {
+                            textureArray[leftIndex] = thisPixel
+                        }
+                        
+                        if currentRight > thisPixel
+                        {
+                            textureArray[rightIndex] = thisPixel
+                        }
+                    }
+                }
             }
         }
+        
+        let bytes:UnsafeRawPointer = UnsafeRawPointer(textureArray)
+        facesTexture.replace(
+            region:region,
+            mipmapLevel:0,
+            withBytes:bytes,
+            bytesPerRow:bytesPerRow)
         
         commandEncoder.setTexture(facesTexture, at:kFacesTextureIndex)
         commandEncoder.setTexture(destinationTexture, at:3)
