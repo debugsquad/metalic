d3db79e839a689cf97c1a4bbbdd40230623afd35
vauxhall <zero.griffin@gmail.com>
2016-10-20 19:18:54 -0500

    renamed extension

---
+++ metalic/Extensions/ExtensionUIColorr.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension UIColor
+{
+    open class var main:UIColor
+    {
+        get
+        {
+            return UIColor(red:0.3, green:0.5, blue:0.7, alpha:1)
+        }
+    }
+    
+    open class var background:UIColor
+    {
+        get
+        {
+            return UIColor(white:0.97, alpha:1)
+        }
+    }
+}
