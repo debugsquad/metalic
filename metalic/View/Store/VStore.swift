1d3eb235063851b11d3a16f95c88e8560dc7eed8
vauxhall <zero.griffin@gmail.com>
2016-10-21 15:49:07 -0500

    flow header size

--- metalic/View/Store/VStore.swift
+++ metalic/View/Store/VStore.swift
@@ -6,6 +6,9 @@ class VStore:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
     weak var viewStore:VStore!
     weak var viewSpinner:VSpinner!
     weak var collectionView:UICollectionView!
+    private let kHeaderSize:CGFloat = 100
+    private let kCellSize:CGFloat = 120
+    private let kInterLine:CGFloat = 1
     
     convenience init(controller:CStore)
     {
@@ -21,6 +24,7 @@ class VStore:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
         self.viewSpinner = viewSpinner
         
         let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize(width:0, height:kHeaderSize)
         
         let collectionView:UICollectionView = UICollectionView(frame:CGRect.zero, collectionViewLayout:flow)
         collectionView.isHidden = false
@@ -74,5 +78,18 @@ class VStore:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICol
         return 1
     }
     
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
     
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VStoreCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VStoreCell.reusableIdentifier,
+            for:indexPath) as! VStoreCell
+        
+        return cell
+    }
 }
