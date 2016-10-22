891291e68a91f3a300db11003f47f0b2e7e9f99e
vauxhall <zero.griffin@gmail.com>
2016-10-22 11:15:40 -0500

    number formatter

--- metalic/Model/Store/MStorePurchase.swift
+++ metalic/Model/Store/MStorePurchase.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class MStorePurchase
 {
-    var mapItems:[String:]
+    var mapItems:[String:MStorePurchaseItem]
+    private let priceFormatter:NumberFormatter
+    
+    init()
+    {
+        mapItems = [:]
+        priceFormatter = NumberFormatter()
+        priceFormatter.numberStyle = NumberFormatter.Style.currencyISOCode
+    }
 }
