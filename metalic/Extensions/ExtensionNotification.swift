019a722075cc47d9551d59dbcdd305721d5a6f55
vauxhall <zero.griffin@gmail.com>
2016-10-22 16:17:04 -0500

    purchases loaded notification

--- metalic/Extensions/ExtensionNotification.swift
+++ metalic/Extensions/ExtensionNotification.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Notification
 {
     static let filtersLoaded:Notification.Name = Notification.Name("filtersLoaded")
+    static let purchasesLoaded:Notification.Name = Notification.Name("purchasesLoaded")
 }
