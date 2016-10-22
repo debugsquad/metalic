9243564efdeb71125a9dfb57c13eeb76822c13b0
vauxhall <zero.griffin@gmail.com>
2016-10-22 16:03:42 -0500

    purchasing status

--- metalic/Model/Store/MStorePurchaseItemStatusPurchasing.swift
+++ metalic/Model/Store/MStorePurchaseItemStatusPurchasing.swift
@@ -1,9 +1,10 @@
-//
-//  MStorePurchaseItemStatusPurchasing.swift
-//  metalic
-//
-//  Created by zero on 10/22/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MStorePurchaseItemStatusPurchasing:MStorePurchaseItemStatus
+{
+    init()
+    {
+        let title:String = NSLocalizedString("MStorePurchaseItemPurchasing_name", comment:"")
+        super.init(title:title)
+    }
+}
