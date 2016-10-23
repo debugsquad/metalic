60dd9fa26f6363a53a38da874e7a1e7ea221d798
vauxhall <zero.griffin@gmail.com>
2016-10-23 17:29:28 -0500

    loader assets

--- metalic/View/Main/VSpinner.swift
+++ metalic/View/Main/VSpinner.swift
@@ -2,13 +2,19 @@ import UIKit
 
 class VSpinner:UIImageView
 {
-    private let kAnimationDuration:TimeInterval = 1
+    private let kAnimationDuration:TimeInterval = 2
     
     init()
     {
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
+            #imageLiteral(resourceName: "assetLoader0"),
+            #imageLiteral(resourceName: "assetLoader1"),
+            #imageLiteral(resourceName: "assetLoader2"),
+            #imageLiteral(resourceName: "assetLoader3"),
+            #imageLiteral(resourceName: "assetLoader4"),
+            #imageLiteral(resourceName: "assetLoader5")
         ]
         
         isUserInteractionEnabled = false
