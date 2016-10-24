5fa7992d65ca245f33302c6efbfcd8aa8c9d92df
vauxhall <zero.griffin@gmail.com>
2016-10-24 10:44:12 -0500

    imageView in cells

--- metalic/Model/Filters/Abstract/MFiltersItemNone.swift
+++ metalic/Model/Filters/Abstract/MFiltersItemNone.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class MFiltersItemNone:MFiltersItem
 {
-    private let kImageName:String = ""
+    private let kImageName:String = "assetFilterNone"
     
     required init()
     {
--- metalic/View/Home/VHomeMenuCell.swift
+++ metalic/View/Home/VHomeMenuCell.swift
@@ -3,6 +3,9 @@ import UIKit
 class VHomeMenuCell:UICollectionViewCell
 {
     weak var label:UILabel!
+    weak var imageView:UIImageView!
+    private let kLabelHeight:CGFloat = 16
+    private let kLabelBottom:CGFloat = 10
     
     override init(frame:CGRect)
     {
@@ -17,12 +20,23 @@ class VHomeMenuCell:UICollectionViewCell
         label.font = UIFont.medium(size:12)
         self.label = label
         
+        let imageView:UIImageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
         addSubview(label)
+        addSubview(imageView)
         
         let views:[String:UIView] = [
-            "label":label]
+            "label":label,
+            "imageView":imageView]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "labelHeight":kLabelHeight,
+            "labelBottom":kLabelBottom]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[label]-0-|",
@@ -30,7 +44,12 @@ class VHomeMenuCell:UICollectionViewCell
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[label]-0-|",
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[imageView]-0-[label(labelHeight)]-(labelBottom)-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -78,6 +97,7 @@ class VHomeMenuCell:UICollectionViewCell
     func config(model:MFiltersItem)
     {
         label.text = model.name
+        imageView.image = UIImage(named:model.asset)
         hover()
     }
 }
