279f916c43b357ba6fba7bfd22519a4da7e909d3
vauxhall <zero.griffin@gmail.com>
2016-10-24 18:48:46 -0500

    controller open var name

--- metalic/Firebase/Analytics/FAnalytics.swift
+++ metalic/Firebase/Analytics/FAnalytics.swift
@@ -3,5 +3,25 @@ import Firebase
 
 class FAnalytics
 {
+    private let kEventScreen:String = "Screen"
     
+    //MARK: public
+    
+    func screen(controller:CController)
+    {
+        let className:NSObject = controller.name as NSObject
+        let eventScreen:NSObject = kEventScreen as NSObject
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        {
+            let parameters:[String:NSObject] = [
+                kFIRParameterItemID:eventScreen,
+                kFIRParameterItemName:className
+            ]
+            
+            FIRAnalytics.logEvent(
+                withName:kFIREventViewItem,
+                parameters:parameters)
+        }
+    }
 }
--- metalic/Firebase/Main/FMain.swift
+++ metalic/Firebase/Main/FMain.swift
@@ -4,10 +4,13 @@ import Firebase
 class FMain
 {
     static let sharedInstance:FMain = FMain()
+    let analytics:FAnalytics
     
     private init()
     {
         FIRApp.configure()
+        
+        analytics = FAnalytics()
     }
     
     //MARK: public
