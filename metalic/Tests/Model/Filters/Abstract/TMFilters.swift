63d1694eb678b2a68a3baf6b70de257be70c7490
vauxhall <zero.griffin@gmail.com>
2016-10-24 20:11:51 -0500

    test purchases list

--- metalic/Tests/Model/Filters/Abstract/TMFilters.swift
+++ metalic/Tests/Model/Filters/Abstract/TMFilters.swift
@@ -1,6 +1,13 @@
 import XCTest
+@testable import metalic
 
 class TMFilters:XCTestCase
 {
-    
+    func testPurchasesList()
+    {
+        let mFilters:MFilters = MFilters()
+        let purchasesList:[[String:String]]? = mFilters.purchasesList()
+        
+        XCTAssertNotNil(purchasesList, "Unabled to load purchases list")
+    }
 }
