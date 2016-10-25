47a1899a4c7b3b4427b8d3fe4d2044fd3a1141a6
vauxhall <zero.griffin@gmail.com>
2016-10-25 15:12:54 -0500

    controller edit with view edit

--- metalic/Controller/Home/CHomeEdit.swift
+++ metalic/Controller/Home/CHomeEdit.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CHomeEdit:CController
 {
+    weak var viewEdit:VHomeEdit!
     
+    override func loadView()
+    {
+        let viewEdit:VHomeEdit = VHomeEdit(controller:self)
+        self.viewEdit = viewEdit
+        view = viewEdit
+    }
 }
