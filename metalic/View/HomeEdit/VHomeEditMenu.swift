b49dc887e49d6e98b2511f8c0d7b1ae3cf68dc1d
vauxhall <zero.griffin@gmail.com>
2016-10-25 15:31:18 -0500

    model edit on view edit menu

--- metalic/View/HomeEdit/VHomeEditMenu.swift
+++ metalic/View/HomeEdit/VHomeEditMenu.swift
@@ -3,12 +3,20 @@ import UIKit
 class VHomeEditMenu:UIView
 {
     weak var controller:CHomeEdit!
+    let model:MEdit
     
-    convenience init(controller:CHomeEdit)
+    init(controller:CHomeEdit)
     {
-        self.init()
+        model = MEdit()
+        
+        super.init(frame:CGRect.zero)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
         self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
     }
 }
