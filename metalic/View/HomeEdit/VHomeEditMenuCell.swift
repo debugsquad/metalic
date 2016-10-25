5b7a7a30ceb78c6c5403e830c08bc1d4dce69a1b
vauxhall <zero.griffin@gmail.com>
2016-10-25 17:28:39 -0500

    image view in menu cell

--- metalic/View/HomeEdit/VHomeEditMenuCell.swift
+++ metalic/View/HomeEdit/VHomeEditMenuCell.swift
@@ -2,11 +2,38 @@ import UIKit
 
 class VHomeEditMenuCell:UICollectionViewCell
 {
+    weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "iamgeView":imageView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
