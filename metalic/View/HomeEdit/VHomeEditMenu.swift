c4a671b11c0bd7043165e577cd3cac748a0bacfb
vauxhall <zero.griffin@gmail.com>
2016-10-25 17:13:14 -0500

    collection view on edit bar

--- metalic/View/HomeEdit/VHomeEditMenu.swift
+++ metalic/View/HomeEdit/VHomeEditMenu.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class VHomeEditMenu:UIView
+class VHomeEditMenu:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     weak var controller:CHomeEdit!
+    weak var collectionView:UICollectionView!
     let model:MEdit
     
     init(controller:CHomeEdit)
@@ -13,10 +14,56 @@ class VHomeEditMenu:UIView
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
         self.controller = controller
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumInteritemSpacing = 0
+        flow.minimumLineSpacing = 0
+        flow.sectionInset = UIEdgeInsets.zero
+        flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        
+        let collectionView:UICollectionView = UICollectionView(frame:CGRect.zero, collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isScrollEnabled = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.bounces = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VHomeEditMenuCell.self,
+            forCellWithReuseIdentifier:
+            VHomeEditMenuCell.reusableIdentifier)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        let views:[String:UIView] = [
+            "collectionView":collectionView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
     }
+    
+    //MARK: collection delegate
+    
+    
 }
