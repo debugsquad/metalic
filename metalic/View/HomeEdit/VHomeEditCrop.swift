f0e38f4d1bc9dd4a457af858ab39b38e2bfed3d3
vauxhall <zero.griffin@gmail.com>
2016-10-26 11:50:19 -0500

    fill overlays

--- metalic/View/HomeEdit/VHomeEditCrop.swift
+++ metalic/View/HomeEdit/VHomeEditCrop.swift
@@ -25,17 +25,33 @@ class VHomeEditCrop:UIView
             borderPosition:VHomeEditCropOverlay.BorderPosition.top)
         let overlayBottom:VHomeEditCropOverlay = VHomeEditCropOverlay(
             borderPosition:VHomeEditCropOverlay.BorderPosition.bottom)
+        let overlayLeftTop:VHomeEditCropOverlay = VHomeEditCropOverlay(
+            borderPosition:VHomeEditCropOverlay.BorderPosition.none)
+        let overlayRightTop:VHomeEditCropOverlay = VHomeEditCropOverlay(
+            borderPosition:VHomeEditCropOverlay.BorderPosition.none)
+        let overlayLeftBottom:VHomeEditCropOverlay = VHomeEditCropOverlay(
+            borderPosition:VHomeEditCropOverlay.BorderPosition.none)
+        let overlayRightBottom:VHomeEditCropOverlay = VHomeEditCropOverlay(
+            borderPosition:VHomeEditCropOverlay.BorderPosition.none)
         
         addSubview(overlayLeft)
         addSubview(overlayRight)
         addSubview(overlayTop)
         addSubview(overlayBottom)
+        addSubview(overlayLeftTop)
+        addSubview(overlayRightTop)
+        addSubview(overlayLeftBottom)
+        addSubview(overlayRightBottom)
         
         let views:[String:UIView] = [
             "overlayLeft":overlayLeft,
             "overlayTop":overlayTop,
             "overlayRight":overlayRight,
-            "overlayBottom":overlayBottom]
+            "overlayBottom":overlayBottom,
+            "overlayLeftTop":overlayLeftTop,
+            "overlayRightTop":overlayRightTop,
+            "overlayLeftBottom":overlayLeftBottom,
+            "overlayRightBottom":overlayRightBottom]
         
         let metrics:[String:CGFloat] = [:]
         
@@ -60,6 +76,26 @@ class VHomeEditCrop:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[overlayLeftTop]-0-[overlayTop]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[overlayTop]-0-[overlayRightTop]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[overlayLeftBottom]-0-[overlayBottom]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[overlayBottom]-0-[overlayRightBottom]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"V:|-0-[overlayTop]",
             options:[],
             metrics:metrics,
@@ -76,6 +112,26 @@ class VHomeEditCrop:UIView
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"V:[overlayTop]-0-[overlayRight]-0-[overlayBottom]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[overlayLeftTop]-0-[overlayLeft]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[overlayRightTop]-0-[overlayRight]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[overlayLeft]-0-[overlayLeftBottom]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[overlayRight]-0-[overlayRightBottom]-0-|",
             options:[],
             metrics:metrics,
             views:views))
